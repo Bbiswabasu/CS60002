28a6e87b8a262e4010962ffde621547601b7c7c4
Bbiswabasu Roy <bbiswabasuroy@Bbiswabasus-MacBook-Air.local>
2024-01-22 15:49:35 +0530

    server file added to directory

--- load_balancer.py
+++ load_balancer.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, g
 import sys
 import os
+import requests
 
 app = Flask(__name__)
 newServerId = 1
@@ -30,34 +31,34 @@ def rep():
 
 @app.route("/add", methods=["POST"])
 def add():
-    g.newServerId = newServerId
+    global newServerId
     payload = request.json
     numberOfServers = payload["n"]
     hostNames = payload["hostnames"]
     for i in range(numberOfServers):
-        hostName = "pub_" + str(g.newServerId)
+        hostName = "pub_" + str(newServerId)
         if (i < len(hostNames)) and (hostNames[i] not in allHosts):
             hostName = hostNames[i]
         allHosts.append(hostName)
         res = os.popen(
-            f"sudo docker run --name {hostName} --network pub --network-alias {hostName} -e serverId={g.newServerId} -d ds_server:latest"
+            f"sudo docker run --name {hostName} --network pub --network-alias {hostName} -e serverId={newServerId} -d ds_server:latest"
         ).read()
         for virtualServer in range(K):
-            pos = phi(g.newServerId, virtualServer)
+            pos = phi(newServerId, virtualServer)
             # Handle when it is full
             while pos in hashToServer.keys():
                 pos += 1
                 pos %= M
-            hashToServer[pos] = g.newServerId
-            if g.newServerId not in serverToHash.keys():
-                serverToHash[g.newServerId] = []
-            serverToHash[g.newServerId].append(pos)
+            hashToServer[pos] = newServerId
+            if newServerId not in serverToHash.keys():
+                serverToHash[newServerId] = []
+            serverToHash[newServerId].append(pos)
         if len(res) == 0:
             print(f"Error starting the container")
             return {"message": "Unable to create server instance(s)!"}, 500
         else:
-            print(f"Container for Server ID: {g.newServerId} started")
-            g.newServerId += 1
+            print(f"Container for Server ID: {newServerId} started")
+            newServerId += 1
     return {
         "message": {"N": len(allHosts), "replicas": allHosts},
         "status": "successful",
@@ -68,6 +69,14 @@ def add():
 def remove():
     pass
 
+@app.route('/<path:path>', methods=['GET'])
+def catch_all(path):
+    hostname = "pub_2"
+    req_url = f'http://{hostname}:5000/{path}'
+    response = requests.get(req_url)
+    print(response.text)
+    return ""
+    # return response.json()
 
 # @app.route("/:i")
 
