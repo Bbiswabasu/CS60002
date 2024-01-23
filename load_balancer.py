7509c9e83c7350cbe02a2509a08f9e4df0f26e3c
Jothi Prakash <suryan535@gmail.com>
2024-01-23 15:09:21 +0530

    Make all - Start Everything, Make clean - Stop Everything. Moved Add Server and Rem Server within Server Object

--- load_balancer.py
+++ load_balancer.py
@@ -14,13 +14,11 @@ class Server:
     
     serverMap=[] # [  { server_id,server_name,virtual_loc } ]
     
-    def __init__(self):
-        self.hashRing=[-1 for _ in range(self.RING_SIZE)]
-
     def __new__(self):
         if not self._instance:
             self._instance=super(Server,self).__new__(self)
-        
+            self.hashRing=[-1 for _ in range(self.RING_SIZE)]
+
         return self._instance
     
     def request_hash(self,i):
@@ -37,17 +35,47 @@ class Server:
         self.hashRing[virtual_hash]=current_server_id
         return virtual_hash
     
-    def removeServers(self,serversToDel,serversRem):
+    def addServers(self,serversToAdd):
+        for new_server_name in serversToAdd:
+            self.SERVER_ID+=1
+
+            virtual_loc=[]
+            for loop in range(0,self.VIRTUAL_INSTANCE):
+                virtual_hash=self.virtual_server_hash(self.SERVER_ID,loop+1)
+                virtual_loc.append(self.vacantRingSpot(self.SERVER_ID,virtual_hash))
+            
+            res = os.popen(f"sudo docker run --name {new_server_name} --network pub --network-alias {new_server_name} -e SERVER_ID={self.SERVER_ID} -d ds_server:latest").read()
+            
+            if len(res) == 0:
+                raise
+                     
+            self.serverMap.append({
+                "server_id":self.SERVER_ID,
+                "server_name":new_server_name,
+                "virtual_loc":virtual_loc
+            })
+
+    def removeServers(self,serversToDel):
         
         # Clean Hash Ring
         for serverIndi in serversToDel:
             for virtual_hash in serverIndi['virtual_loc']:
                 self.hashRing[virtual_hash]=-1
         
+        # Stopping the containers
+        for serverIndi in serversToDel:
+            res = os.popen(f"sudo docker stop {serverIndi['server_name']} && sudo docker rm {serverIndi['server_name']}").read()
+            
+            if len(res) == 0:
+                raise
+        
+        serversRem=[serverIndi for serverIndi in self.serverMap if serverIndi not in serversToDel]
         self.serverMap=serversRem
 
+
     def __del__(self):
-        self.removeServers(self.serverMap, [])
+        self.removeServers(self.serverMap)
+    
 
 @app.route("/rep", methods=["GET"])
 def rep():
@@ -84,24 +112,8 @@ def add():
             tmp_server_id+=1
             payload['hostnames'].append(f'pub_server_{tmp_server_id}')
         
-        for new_server_name in payload["hostnames"]:
-            server.SERVER_ID+=1
+        server.addServers(payload['hostnames'])
 
-            virtual_loc=[]
-            for loop in range(0,server.VIRTUAL_INSTANCE):
-                virtual_hash=server.virtual_server_hash(server.SERVER_ID,loop+1)
-                virtual_loc.append(server.vacantRingSpot(server.SERVER_ID,virtual_hash))
-            
-            res = os.popen(f"sudo docker run --name {new_server_name} --network pub --network-alias {new_server_name} -e SERVER_ID={server.SERVER_ID} -d ds_server:latest").read()
-            
-            if len(res) == 0:
-                raise
-                     
-            server.serverMap.append({
-                "server_id":server.SERVER_ID,
-                "server_name":new_server_name,
-                "virtual_loc":virtual_loc
-            })
         return rep()
     except:
         response={
@@ -133,18 +145,9 @@ def rem():
 
             if serverIndi['server_name'] not in serversToDel:
                 serversToDel.append(serverIndi)
-        
-        serversRem=[serverIndi for serverIndi in server.serverMap if serverIndi not in serversToDel]
          
         # Remove Servers and Clean up the state
-        server.removeServers(serversToDel,serversRem)
-        
-        # Stopping the containers
-        for serverIndi in serversToDel:
-            res = os.popen(f"sudo docker stop {serverIndi['server_name']}").read()
-            
-            if len(res) == 0:
-                raise
+        server.removeServers(serversToDel)
         
         return rep()
     except:
@@ -161,6 +164,6 @@ def catch_all(path):
     req_url = f'http://{hostname}:5000/{path}'
     response = requests.get(req_url)
     return response.json(), response.status_code
-    
+
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
