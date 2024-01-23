4241aae38c72171bf9af7c2854201a81e0234211
Jothi Prakash <suryan535@gmail.com>
2024-01-23 23:32:47 +0530

    Fixed Issue of Server not handling multiple concurrent requests

--- load_balancer.py
+++ load_balancer.py
@@ -189,4 +189,4 @@ def balancer(path):
         return jsonify(response),400
     
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
