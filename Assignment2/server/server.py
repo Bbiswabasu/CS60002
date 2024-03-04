5807354720d07fdbfc10df9cf6a12ae0fe0a18f8
Jothi Prakash <suryan535@gmail.com>
2024-03-04 15:35:06 +0530

    Added Config

--- Assignment2/server/server.py
+++ Assignment2/server/server.py
@@ -1,21 +1,59 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
 
+class DB:
+    DB_instance={}
+    _instance=None
 
-@app.route("/home")
-def home():
-    SERVER_ID = os.environ["SERVER_ID"]
+    def __new__(self):
+        if not self._instance:
+            self._instance = super(DB, self).__new__(self)
+        return self._instance
+    
+    def addShard(self,shardName):
+        self.DB_instance[shardName]=[]
+    
+    def getStatus(self):
+        shardNames=[]
 
-    response = {"message": f"Hello from Server: {SERVER_ID}", "status": "successful"}
-    return jsonify(response), 200
+        for shard in self.DB_instance.keys():
+            shardNames.append(shard)
+        
+        return shardNames
 
 
-@app.route("/heartbeat")
+@app.route("/config",methods=["POST"])
+def config():
+    payload=request.json
+    
+    db=DB()
+    for shardName in payload['shards']:
+        db.addShard(shardName)
+    
+    shards=db.getStatus()
+    
+    message=""
+
+    for shardName in shards:
+        message+=f"Server0: {shardName}, "
+    
+    message+="configured"
+
+    response={
+        "message":message,
+        "status":'successful'
+    }
+
+    return response,200
+
+
+
+@app.route("/heartbeat",methods=["GET"])
 def heartbeat():
     return jsonify({}), 200
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(debug=True,host="0.0.0.0", port=5000)
