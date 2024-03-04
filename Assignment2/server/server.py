3d5da79d609fc0c51b7c5b4dbbe37dee454f55db
Jothi Prakash <suryan535@gmail.com>
2024-03-04 16:15:22 +0530

    /read and /copy endpoint added

--- Assignment2/server/server.py
+++ Assignment2/server/server.py
@@ -10,6 +10,15 @@ class DB:
     def __new__(self):
         if not self._instance:
             self._instance = super(DB, self).__new__(self)
+
+            self.DB_instance={
+                "sh1":[
+                    {"stud_id":1232,"Stud_name":"ABC","Stud_marks":25}
+                ],
+                "sh2":[
+                    {"stud_id":2255,"Stud_name":"GHI","Stud_marks":27}
+                ]
+            }
         return self._instance
     
     def addShard(self,shardName):
@@ -22,6 +31,25 @@ class DB:
             shardNames.append(shard)
         
         return shardNames
+    
+    def getData(self,shardName):
+        if shardName in self.DB_instance:
+            return {shardName:self.DB_instance[shardName]}
+        
+        return {shardName:[]}
+    
+    def getRecords(self,query):
+        
+        shardName=query["shard"]
+        if shardName not in self.DB_instance:
+            return []
+        
+        id_low=int(query["stud_id"]["low"])
+        id_high=int(query["stud_id"]["high"])
+
+        result=[record for record in self.DB_instance[shardName] if record['stud_id']>=id_low and record['stud_id']<=id_high]
+
+        return result
 
 
 @app.route("/config",methods=["POST"])
@@ -48,6 +76,40 @@ def config():
 
     return response,200
 
+
+@app.route("/copy",methods=["GET"])
+def copy():
+    payload=request.json
+
+    db=DB()
+    
+    message=[]
+
+    for shardName in payload["shards"]:
+        message.append(db.getData(shardName))
+    
+    response={
+        "message":message,
+        "status":"success"
+    }
+    
+    return response,200
+
+
+@app.route("/read",methods=["GET"])
+def read():
+    payload=request.json
+
+    db=DB()
+
+    data=db.getRecords(payload)
+
+    response={
+        "data":data,
+        "status":"success"
+    }
+
+    return response,200
 
 
 @app.route("/heartbeat",methods=["GET"])
